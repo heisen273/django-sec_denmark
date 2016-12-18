fa770f17b093c91f3763f2b8504ae36857804991
Anton Antonenko <aantonenko@jerasoft.net>
2016-12-19 01:56:27 +0200

    database for backup

---
+++ backup/database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE django_sec_lol
+(
+      value_name character varying(200) NOT NULL,
+      value character varying(200) NOT NULL,
+      start_date date NOT NULL,
+      end_date date,
+      company_id integer NOT NULL,
+      doc_type character varying(200) NOT NULL
+)
